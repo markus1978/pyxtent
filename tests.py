cb7477f2b012556568a85de1bf78dda233d55ced
Markus Scheidgen <markus.scheidgen@gmail.com>
2021-12-12 17:35:54 +0100

    Added parser.

--- tests.py
+++ tests.py
@@ -1,6 +1,6 @@
 import pytest
 
-from xtend import scan
+from xtend import Parser, parse, scan
 
 
 @pytest.mark.parametrize('input, output', [
@@ -17,6 +17,33 @@ from xtend import scan
     pytest.param('1{{2}}3', 'string', id='escape')
 ])
 def test_scanner(input: str, output: str):
+    parser = Parser(input)
+
     result_output = [token for token, _ in scan(input)]
+    expected_output = [token.strip() for token in output.split(',')]
+    assert result_output == expected_output
+
+
+@pytest.mark.parametrize('input, output', [
+    pytest.param('class', 'string', id='string'),
+    pytest.param('class {name}:', 'string, expr, string', id='expr'),
+    pytest.param('class name{IF base}(base){END}:', 'string, if, string', id='if'),
+    pytest.param('{IF c}s{ELIF c}s{END}', 'if', id='if-elif'),
+    pytest.param('{IF c}s{ELSE}s{END}', 'if', id='if-else'),
+    pytest.param('{IF c}s{ELSE}s{ELSE}s{END}', None, id='if-double-else'),
+    pytest.param('{IF c}s{ELSE}s', None, id='if-no-end'),
+    pytest.param('{IF c}{END}', None, id='if-no-stmt'),
+    pytest.param('{IF }s{END}', None, id='if-no-code'),
+    pytest.param('{IF c}s{ELIF c}{END}', None, id='elif-no-stmt'),
+    pytest.param('{IF c}s{ELSE}{END}', None, id='else-no-stmt'),
+    pytest.param('{FOR item IN list}{item}{END}', 'for', id='for'),
+])
+def test_parser(input: str, output: str):
+    if output is None:
+        with pytest.raises(Exception):
+            list(parse(input))
+        return
+
+    result_output = [token for token, _ in parse(input)]
     expected_output = [token.strip() for token in output.split(',')]
     assert result_output == expected_output
--- xtend.py
+++ xtend.py
@@ -1,3 +1,4 @@
+from typing import Generator, Tuple, Union, List
 import re
 
 tokens = {
@@ -14,7 +15,7 @@ token_pattern = re.compile('|'.join([
 ]))
 
 
-def scan(input: str):
+def scan(input: str) -> Generator[Tuple[str, str], None, None]:
     state = 'string'
     current_string = []
 
@@ -42,13 +43,13 @@ def scan(input: str):
                 continue
 
             if len(current_string) > 0:
-                yield ('code', current_string)
+                yield 'code', ''.join(current_string)
                 current_string = []
 
             if token == 'close':
                 state = 'string'
             elif token == 'keyword':
-                yield ('keyword', value)
+                yield 'keyword', value
             else:
                 raise NotImplementedError()
 
@@ -57,3 +58,121 @@ def scan(input: str):
             yield ('string', ''.join(current_string))
         else:
             yield ('code', ''.join(current_string))
+
+
+class Parser():
+    def __init__(self, input):
+        self.input = scan(input)
+        self.lookahead = None
+
+    def fail(self, expected=Union[str, List[str]]):
+        raise Exception('cannot parse')
+
+    def next(self) -> Tuple[str, str]:
+        if self.lookahead:
+            next_item = self.lookahead
+            self.lookahead = None
+            return next_item
+        return next(self.input, (None, None))
+
+    def peek(self) -> Tuple[str, str]:
+        if not self.lookahead:
+            self.lookahead = self.next()
+        return self.lookahead
+
+    def parse_keyword(self, keyword: str):
+        token, value = self.next()
+        if token != 'keyword' and value != keyword:
+            self.fail(expected=keyword)
+
+    def parse_code(self):
+        token, value = self.next()
+        if token == 'code':
+            if value.strip() == '':
+                self.fail(expected='code')
+            return 'code', value
+        self.fail(expected='code')
+
+    def parse_string(self):
+        string_value = []
+        while True:
+            token, value = self.peek()
+            if token in ['string', 'indent', 'nl']:
+                self.next()
+                string_value.append(value)
+
+            else:
+                break
+
+        if len(string_value) == 0:
+            self.fail(expected='string')
+
+        return 'string', ''.join(string_value)
+
+    def parse_xtend(self):
+        while self.peek()[0]:
+            yield self.parse_stmt()
+
+    def parse_stmt(self):
+        # stmt -> string | if | for | expr
+        token, value = self.peek()
+        if token == 'keyword':
+            if value == 'IF':
+                return self.parse_if()
+
+            if value == 'FOR':
+                return self.parse_for()
+
+            self.fail(expected=['if', 'for'])
+
+        if token == 'string':
+            return self.parse_string()
+
+        if token == 'code':
+            return self.parse_expr()
+
+        self.fail(expected=['if', 'for', 'string', 'code'])
+
+    def parse_expr(self):
+        return 'expr', self.parse_code()
+
+    def parse_if(self):
+        # if -> IF CODE stmt (ELIF CODE stmt)* (ELSE stmt)? END
+        results = []
+        self.parse_keyword('IF')
+        results.append((self.parse_code(), self.parse_stmt()))
+
+        while True:
+            token, value = self.peek()
+            if token == 'keyword':
+                if value == 'ELIF':
+                    self.next()
+                    results.append((self.parse_code(), self.parse_stmt()))
+                    continue
+                elif value == 'ELSE':
+                    self.next()
+                    results.append((self.parse_stmt()))
+                    break
+                elif value == 'END':
+                    break
+
+                self.fail(expected=['ELIF', 'ELSE', 'END'])
+
+        self.parse_keyword('END')
+
+        return 'if', results
+
+    def parse_for(self):
+        # for -> FOR code IN code (SEPERATOR code)? stmt END
+        self.parse_keyword('FOR')
+        item_code = self.parse_code()
+        self.parse_keyword('IN')
+        list_code = self.parse_code()
+        stmt = self.parse_stmt()
+        self.parse_keyword('END')
+
+        return 'for', (item_code, list_code, stmt)
+
+
+def parse(input: str):
+    return Parser(input).parse_xtend()
